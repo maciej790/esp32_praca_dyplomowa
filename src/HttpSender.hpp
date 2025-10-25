02954084aa5596b388d9a0a19c7549be1b4218c9
maciej790 <maciejmw126@gmail.com>
2025-10-25 17:38:54 +0200

    done send data

---
+++ src/HttpSender.hpp
@@ -0,0 +1,82 @@
+// HttpSender.hpp
+#pragma once
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino.h>
+
+class HttpSender
+{
+private:
+    String serverUrl;
+
+public:
+    HttpSender(const String &url) : serverUrl(url) {}
+
+    void begin(const char *ssid, const char *password)
+    {
+        Serial.print("Łączenie z WiFi...");
+        WiFi.begin(ssid, password);
+
+        int max_attempts = 20;
+        int attempts = 0;
+
+        while (WiFi.status() != WL_CONNECTED && attempts < max_attempts)
+        {
+            delay(500);
+            Serial.print(".");
+            attempts++;
+        }
+
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            Serial.println("\nPołączono z WiFi!");
+            Serial.print("Adres IP: ");
+            Serial.println(WiFi.localIP());
+        }
+        else
+        {
+            Serial.println("\nNie udało się połączyć z WiFi.");
+        }
+    }
+
+    bool sendData(float temperature, float humidity, float pressure, const String &airQuality)
+    {
+        if (WiFi.status() != WL_CONNECTED)
+        {
+            Serial.println("Brak połączenia z WiFi!");
+            return false;
+        }
+
+        HTTPClient http;
+        http.begin(serverUrl);
+        http.addHeader("Content-Type", "application/json");
+
+        String jsonPayload = "{";
+        jsonPayload += "\"temperature\": " + String(temperature, 2) + ",";
+        jsonPayload += "\"humidity\": " + String(humidity, 2) + ",";
+        jsonPayload += "\"pressure\": " + String(pressure, 2) + ",";
+        jsonPayload += "\"airQuality\": \"" + airQuality + "\"";
+        jsonPayload += "}";
+
+        Serial.println("Wysyłam JSON:");
+        Serial.println(jsonPayload);
+
+        int httpResponseCode = http.POST(jsonPayload);
+
+        if (httpResponseCode > 0)
+        {
+            Serial.print("Dane wysłane, HTTP code: ");
+            Serial.println(httpResponseCode);
+            Serial.println("Odpowiedź serwera: " + http.getString());
+            http.end();
+            return true;
+        }
+        else
+        {
+            Serial.print("Błąd wysyłania: ");
+            Serial.println(httpResponseCode);
+            http.end();
+            return false;
+        }
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,54 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include "Bme280.hpp"
 #include "GroveMP503.hpp"
 #include "Lcd.hpp"
+#include "HttpSender.hpp"
 
 const int SDA_PIN = 21;
 const int SCL_PIN = 22;
-const int SIGNAL_PIN = 35; 
+const int SIGNAL_PIN = 35;
+
+const char *ssid = "vnet-894ED2";
+const char *password = "001dd2894ed2";
 
 Bme280 bme280;
 GroveMP503 groveMp503;
 Lcd lcd;
+HttpSender sender("http://ptsv3.com/stacjaPogodowa");
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   delay(500);
 
   bme280.begin(SDA_PIN, SCL_PIN);
   groveMp503.begin(SIGNAL_PIN);
   lcd.begin();
-
+  sender.begin(ssid, password);
 }
 
-void loop() {
-  // Pobierz dane z czujników
+void loop()
+{
   Bme280Data envData = bme280.readValues();
   GroveMP503Data airData = groveMp503.readAirQuality();
 
-  // Wypisz dane na Serial
-  Serial.print("Temperatura: "); Serial.print(envData.temperature); Serial.println(" °C");
-  Serial.print("Ciśnienie: "); Serial.print(envData.pressure); Serial.println(" hPa");
-  Serial.print("Wilgotność: "); Serial.print(envData.humidity); Serial.println(" %");
-  Serial.print("Jakość powietrza: "); Serial.println(airData.quality.c_str());
+  Serial.print("Temperatura: ");
+  Serial.println(envData.temperature);
+  Serial.print("Ciśnienie: ");
+  Serial.println(envData.pressure);
+  Serial.print("Wilgotność: ");
+  Serial.println(envData.humidity);
+  Serial.print("Jakość powietrza: ");
+  Serial.println(airData.quality.c_str());
   Serial.println("------------------------");
 
-  // Wyświetl dane na LCD
   lcd.displayData(envData.temperature, envData.humidity, envData.pressure, String(airData.quality.c_str()));
+
+  if (!sender.sendData(envData.temperature, envData.humidity, envData.pressure, String(airData.quality.c_str())))
+  {
+    Serial.println("Błąd wysyłania danych do serwera!");
+  }
 
   delay(3000);
 }
